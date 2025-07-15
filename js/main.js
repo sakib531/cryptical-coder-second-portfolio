ae49535af2e96fff9aa8a200c2e4e831962449fa
sakib531 <mdsakibahammad531@gmail.com>
2025-07-15 23:50:04 +0600

    progress bar js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@ let navbar = document.querySelector(".navbar");
 window.onscroll = () =>{
     this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
 }
+
+const skills_wrap = document.querySelector(".about-skills"),
+      skills_bar = document.querySelectorAll(".progress-line");
+      window.addEventListener("scroll", () => {
+        checkScroll(skills_wrap);
+      })
