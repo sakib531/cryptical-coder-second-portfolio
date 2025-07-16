c0d8a161be9f0d6f9f44e575ecc1b4cfacabdcd8
sakib531 <mdsakibahammad531@gmail.com>
2025-07-16 23:52:59 +0600

    progress bsr js , service section start

--- js/main.js
+++ js/main.js
@@ -3,8 +3,18 @@ window.onscroll = () =>{
     this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
 }
 
-const skills_wrap = document.querySelector(".about-skills"),
+const skills_wrap = document.querySelector(".about-skill"),
       skills_bar = document.querySelectorAll(".progress-line");
       window.addEventListener("scroll", () => {
-        checkScroll(skills_wrap);
+        skillsEffect();
       })
+
+      function checkScroll(el){
+        let rect = el.getBoundingClientRect();
+        if(window.innerHeight >= rect.top + el.offsetHeight) return true;
+        return false;
+      }
+      function skillsEffect(){
+        if(!checkScroll(skills_wrap)) return;
+        skills_bar.forEach((skill) => (skill.style.width = skill.dataset.progress));
+      }
